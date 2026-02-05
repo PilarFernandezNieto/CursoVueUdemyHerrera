1029779ae156f619d9d0bb5b67cb600f94b2ddcf
PilarFernandezNieto <apreilmc@gmail.com>
2026-02-05 20:18:10 +0100

    estructura y daysiui

--- projects-app/vite.config.ts
+++ projects-app/vite.config.ts
@@ -1,6 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
