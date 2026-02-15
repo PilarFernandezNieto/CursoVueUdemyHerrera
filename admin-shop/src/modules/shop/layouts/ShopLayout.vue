e781b6086b18bdff5948d317a237819b702c7479
PilarFernandezNieto <apreilmc@gmail.com>
2026-02-15 20:40:07 +0100

    primeras vistas y separacion componentes

---
+++ admin-shop/src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- component -->
+  <!-- Create By Joker Banny -->
+
+  <!-- Header Navbar -->
+  <TopMenu />
+  <!-- End Header Navbar -->
+
+  <RouterView />
+
+  <!-- Footer -->
+  <CustomFooter />
+  <!-- End Footer -->
+</template>
+
+<script setup lang="ts">
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
+
+<style scoped></style>
