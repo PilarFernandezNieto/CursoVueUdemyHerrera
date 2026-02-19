8f0bbdd6998167ac7a4c6725ca04053dcbeedf9d
PilarFernandezNieto <apreilmc@gmail.com>
2026-02-19 21:22:14 +0100

    Login

--- admin-shop/src/modules/auth/storres/auth.store.ts
+++ admin-shop/src/modules/auth/storres/auth.store.ts
@@ -2,11 +2,12 @@ import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
 import { AuthStatus, type User } from '../interfaces';
 import { loginAction } from '../actions';
+import { useLocalStorage } from '@vueuse/core';
 
 export const useAuthStore = defineStore('auth', () => {
   const authStatus = ref(AuthStatus.Checking);
   const user = ref<User | undefined>();
-  const token = ref('');
+  const token = ref(useLocalStorage('token', ''));
 
   const login = async (email: string, password: string) => {
     try {
