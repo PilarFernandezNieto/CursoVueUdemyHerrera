6134e80dfd293dde58e9b18abbf536a4172eae5b
PilarFernandezNieto <apreilmc@gmail.com>
2026-03-22 21:39:58 +0100

    Admin Dashboard

--- admin-shop/src/modules/admin/routes/index.ts
+++ admin-shop/src/modules/admin/routes/index.ts
@@ -7,13 +7,5 @@ export const adminRoutes: RouteRecordRaw = {
   path: '/admin',
   name: 'admin',
   beforeEnter: [isAuthenticatedGuard, isAdminGuard],
-  redirect: { name: 'admin-dashboard' },
   component: () => import('@/modules/admin/layouts/AdminLayout.vue'),
-  children: [
-    {
-      path: 'dashboard',
-      name: 'admin-dashboard',
-      component: () => import('@/modules/admin/views/DashboardView.vue'),
-    },
-  ],
 };
