8a609c54c4f51b5455e54ba5cb62200141dca517
PilarFernandezNieto <apreilmc@gmail.com>
2026-03-22 21:06:43 +0100

    Spinner

--- admin-shop/src/App.vue
+++ admin-shop/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <FullScreenLoader v-if="authStore.isChecking" />
   <RouterView />
   <VueQueryDevtools />
 </template>
@@ -7,6 +8,7 @@ import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
 import { useAuthStore } from './modules/auth/stores/auth.store';
 import { AuthStatus } from './modules/auth/interfaces';
 import { useRoute, useRouter } from 'vue-router';
+import FullScreenLoader from './modules/common/components/FullScreenLoader.vue';
 
 const authStore = useAuthStore();
 const router = useRouter();
